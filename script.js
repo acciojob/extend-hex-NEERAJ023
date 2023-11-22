2bfcc46ac94ab942b0b02fe8ea7666cb87820d4b
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-22 18:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let str="";
+	for(let i=0; i<shortHex.length; i++){
+		if(shortHex[i]=="#"){
+
+			str+="#";
+		}
+		else{
+			if(str==""){
+			str+="#";
+				
+				str+=shortHex[i];
+				str+=shortHex[i];
+		 }else{
+				str+=shortHex[i];
+				str+=shortHex[i];
+		 }
+		}
+	}
+	return str;
 };
 
 // Do not change the code below.
